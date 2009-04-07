9a047130c6f8b17ca99d4e3f1de7ffac0539c035
Joni Freeman <joni.freeman@ri.fi>
2009-04-07 20:41:35 +0300

    initial webserver

---
+++ sbt
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+scala -cp project/sbt-0.3.7.jar sbt.Main $*
---
+++ src/main/scala/spmd/spmd.scala
@@ -0,0 +1,57 @@
+package spmd
+
+
+/*
+ * http://www.nabble.com/epmd-not-cleaning-up-node-name-if-node-is-down.-td18996856.html
+The normal and correct behaviour is that a node is unregistered from
+epmd when it is shutdown.
+each Erlang node that register to epmd establishes and keep an open
+tcp connection towards epmd.
+When that connection is broken the Erlang node is unregistered.
+*/
+
+object Daemon {
+  def main(args: Array[String]) = {
+    // Start listening, http?
+    Server
+    // Establish client conn
+    // API, json?
+    // Integrate to console
+  }
+}
+
+trait Client {
+//  def names = names(inet.getHostName)
+//  def names(host: HostName)
+//  def registerNode(name: Name, port: Int)
+}
+
+// PUT /nodes/{node}/{ip}/{port} HTTP/1.1
+object Server {
+  import java.io._
+  import java.net._
+  import scala.io.Source
+
+  val serverSocket = new ServerSocket(6128)
+  val clientSocket = serverSocket.accept
+  val out = new PrintWriter(clientSocket.getOutputStream, true)
+  val in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream))
+  val request = Request.fromRequestLine(in.readLine)
+  println(request)
+
+  case class Request(method: Method, url: String)
+  case object Request {
+    def fromRequestLine(s: String) = {
+      val elems = s.split(" ")
+      (elems(0), elems(1)) match {
+        case ("PUT", url) => Request(PUT, url)
+        case ("GET", url) => Request(GET, url)
+        case _ => error("unknown method: " + s)
+      }
+    }
+  }
+  
+  sealed abstract class Method
+  case object PUT extends Method
+  case object GET extends Method
+}
